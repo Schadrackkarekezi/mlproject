d6ca99d1b01d50c3404715418d748d2d07022b76
Schadrack Karekezi <vitalshadrackk@gmail.com>
2026-01-13 17:05:34 -0600

    EDA and model training notebook

--- src/exception.py
+++ src/exception.py
@@ -15,8 +15,6 @@ class CustomException(Exception):
         self.error_message=error_message_details(error_message, error_detail=error_detail)
     def __str__(self):
         return self.error_message
-    
-
 
 
 
